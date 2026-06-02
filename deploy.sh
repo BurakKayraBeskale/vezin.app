762fa3aa8008859cfc66083ab6c907bd417dbde7
BurakKayraBeskale <kkoklor@gmail.com>
2026-06-02 10:31:38 +0300

    deploy env koruma

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 cd /var/www/vezin
+
+# .env dosyasını yedekle
+cp .env .env.backup
+
+# git güncelle
 git stash
 git pull origin main
+
+# .env dosyasını geri yükle
+cp .env.backup .env
+
 npm install
 npm run build
-pm2 restart vezin
+pm2 restart vezin --update-env
+
 echo "Deploy tamamlandı!"
